7f9cc1a53d6f79a6149a88bcd1f5608a94c1e49c
KrashAnarhia <krashanarhia@gmail.com>
2021-11-13 16:23:20 +0700

    mkdir static, update map.html

--- backapp/routes.py
+++ backapp/routes.py
@@ -12,6 +12,19 @@ def index():
 def create_map():
     return render_template("map.html")
 
+@app.route('/parse_test')
+def test():
+    workbook = load_workbook('C:\\Temp\\1.xlsx', read_only=True)
+    first_sheet = workbook.get_sheet_names()[0]
+    worksheet = workbook.get_sheet_by_name(first_sheet)
+
+    for row in worksheet.iter_rows():
+        print(row)
+
+    # check out the last row
+    for cell in row:
+        print(cell)
+    return True
 @app.route('/uploadegrul')
 def uploadegrul():
     Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
