7748596b9b98ddb95fcc97461fa7b7793b6c6544
KrashAnarhia <krashanarhia@gmail.com>
2021-11-13 12:04:37 +0700

    Update backend

--- backapp/__inti__.py
+++ backapp/__inti__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+
 from sqlalchemy.ext.declarative import declarative_base
 from config import config, configBD
 
--- backapp/app.py
+++ backapp/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import render_template, url_for, jsonify, abort
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'AgroHacaton 2021!'
 
 @app.route('/map')
 def create_map():
