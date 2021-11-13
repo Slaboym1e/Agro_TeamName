d2d4e49b79414e0bca2f8e4f1c5092c2553a3099
kitekat <sasha2003161@gmail.com>
2021-11-13 18:40:11 +0700

    Добавлены метки

--- backapp/static/js/map.js
+++ backapp/static/js/map.js
@@ -77,140 +77,245 @@ ymaps.ready(function () {
     myPolygon.editor.startDrawing();
     //////////////////////////////////////////////////////////////////////////
 
+    let getoobjects=[];
 
-     myGeoObject = new ymaps.GeoObject({
+
+
+
+            myGeoObject = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.153395, 61.398763]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 65',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
+        });getoobjects.push(myGeoObject);
 
-          myGeoObject1 = new ymaps.GeoObject({
+
+          console.log(getoobjects);
+        myGeoObject1 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.153406, 61.399230]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 63Вк1',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject1);
+        myGeoObject2 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.152845, 61.399140]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 65/1',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject2);
+            myGeoObject3 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.152562, 61.398888]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 65А',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject3);
+            myGeoObject4 = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.153406, 61.399230]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 63Вк1',
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+        });getoobjects.push(myGeoObject4);
+            myGeoObject5 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.151718, 61.399167]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 73/1',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject5);
+            myGeoObject6 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.151343, 61.399050]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 73',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject6);
+            myGeoObject7 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.150967, 61.399167]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 75',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [55.153395, 61.398763]
-            },
-        }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject7);
+            myGeoObject8 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.150587, 61.399086]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 77',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
-
-          myGeoObject = new ymaps.GeoObject({
+        });getoobjects.push(myGeoObject8);
+            myGeoObject9 = new ymaps.GeoObject({
             // Описание геометрии.
             geometry: {
                 type: "Point",
                 coordinates: [55.149892, 61.399149]
             },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 79',
+            }
         }, {
             // Опции.
             // Иконка метки будет растягиваться под размер ее содержимого.
             preset: 'islands#blackStretchyIcon',
-        }),
+        });getoobjects.push(myGeoObject9);
+            myGeoObject10 = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.149177, 61.399185]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 81А',
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+        });getoobjects.push(myGeoObject10);
+            myGeoObject11 = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.149033, 61.399922]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent: 'улица Елькина, 81Б',
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+        });getoobjects.push(myGeoObject11);
+            myGeoObject12 = new ymaps.GeoObject({
+            // Описание геометрии.
+            geometry: {
+                type: "Point",
+                coordinates: [55.149414, 61.399975]
+            },
+            // Свойства.
+            properties: {
+                // Контент метки.
+                hintContent1: 'улица Елькина, 81',
+            }
+        }, {
+            // Опции.
+            // Иконка метки будет растягиваться под размер ее содержимого.
+            preset: 'islands#blackStretchyIcon',
+        });getoobjects.push(myGeoObject12);
+    for (let i in getoobjects)
+    { // выведет 0, затем 1, затем 2
+         myMap.geoObjects.add(getoobjects[i]);
 
+    }
 
-
-
-
-
-
-
-
-
-
-    myMap.geoObjects
-        .add(myGeoObject)
-        .add(myGeoObject1)
-       // .add(myPlacemark)
-        //.add(myPlacemarkWithContent);
+//     myMap.geoObjects
+//         .add(myGeoObject)
+//         .add(myGeoObject1)
+//         .add(myGeoObject2)
+//         .add(myGeoObject3)
+//         .add(myGeoObject4)
+//         .add(myGeoObject5)
+//         .add(myGeoObject6)
+//         .add(myGeoObject7)
+//         .add(myGeoObject8)
+//         .add(myGeoObject9)
+//         .add(myGeoObject10)
+//         .add(myGeoObject11)
+//         .add(myGeoObject12);
+//         //.add(myGeoObject13)
+//         //.add(myGeoObject14)
+//         //.add(myGeoObject1)
+//        // .add(myPlacemark)
+//         //.add(myPlacemarkWithContent);
 });
